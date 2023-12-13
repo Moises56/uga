e532f265dcdcc9ffbd56697afd8280d672c21a2b
moises0814 <144181018+moises0814@users.noreply.github.com>
2023-12-13 15:55:46 -0600

    styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 export const routes: Routes = [
   // { path: '', redirectTo: '/inicio', pathMatch: 'full'},
   { path: '', title:'inicio', component: InicioComponent},
   { path: 'login', title:'login', component: LoginComponent},
   { path: 'registro', title:'registro', component: RegistroComponent},
+  { path: 'admin', title: 'admin', component: AdminComponent}
 ];
--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-section',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './section.component.html',
   styleUrl: './section.component.css'
 })
